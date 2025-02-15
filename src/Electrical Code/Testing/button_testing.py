38a4e91f2de14a10f830f447c92e7072461418cf
AlbertKalayil <79732912+AlbertKalayil@users.noreply.github.com>
2025-02-15 16:09:37 -0700

    Maintains speed when button is not pressed

--- src/Electrical Code/Testing/button_testing.py
+++ src/Electrical Code/Testing/button_testing.py
@@ -31,17 +31,33 @@ green_pressed = 0
 red_pressed = 0
         
 #Use your Phidgets | This code will turn on the LED when the matching button is pressed and turns off the LED when the matching button is released. The sleep function slows down the loop so the button state is only checked every 150ms.
-while(1):
+
+def getRed_or_Green():
+    global red_pressed, green_pressed  # Declare the variables as global
     if(redButton.getState()):
-         red_pressed+=1
-         redLED.setState(True)
-         print("red is pressed")
+        red_pressed += 1
+        redLED.setState(True)
+        return 'red'
     else:
         redLED.setState(False)
     if(greenButton.getState()):
-        green_pressed+=1
+        green_pressed += 1
         greenLED.setState(True)
-        print("green is pressed")
+        return 'green'
     else:
         greenLED.setState(False)
-    time.sleep(0.15)
+        return 'none'
+
+
+if __name__ == "__main__":
+    speed = 0
+    while True:
+        button_colour = getRed_or_Green()
+
+        if button_colour == 'green':  # Speed up (A key)
+            speed = min(15, speed + 0.1)  # Cap speed at 15
+        if button_colour == 'red':  # Slow down (D key)
+            speed = max(1, speed - 0.1)  # Prevent speed from going below 1
+
+        if button_colour == 'lol':
+            print(speed)
