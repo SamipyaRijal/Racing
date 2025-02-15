de018eb42a0bd0bb51f9eb0848339de88b74f083
SamipyaRijal <147675714+SamipyaRijal@users.noreply.github.com>
2025-02-15 14:57:20 -0700

    Update button testing.py

--- src/Electrical Code/Testing/button testing.py
+++ src/Electrical Code/Testing/button testing.py
@@ -44,14 +44,7 @@ while(1):
         print("green is pressed")
     else:
         greenLED.setState(False)
-    if(green_pressed == 10 or red_pressed == 10):
-         break
     time.sleep(0.15)
 
-if(red_pressed==10):
-    for i in range(0,11):
-        redLED.setState(True)
-        time.sleep(1)
-        redLED.setState(False)
-        time.sleep(1)
+
         
